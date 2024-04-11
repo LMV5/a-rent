8380cffd378217607c275d06157b6df9401f2163
Marina <marina.lap08@gmail.com>
2024-04-11 22:13:34 +0200

    add active links and conditional rendering

--- components/LinkButton.jsx
+++ components/LinkButton.jsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
 
-function LinkButton({ children, href, type }) {
+function LinkButton({ children, href, type, active }) {
   const base =
-    "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
+    "text-white hover:bg-gray-800 hover:text-white rounded-md px-3 py-2";
 
   const styles = {
     primary: base + " py-3 px-4 md:px-6 md:py-4",
-    mobileMenu:
-      "bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium",
+    mobileMenu: "text-white block rounded-md px-3 py-2 text-base font-medium",
   };
 
   if (href)
     return (
-      <Link href={href} className={styles[type]}>
+      <Link href={href} className={`${styles[type]} ${active}`}>
         {children}
       </Link>
     );
--- components/MobileMenu.jsx
+++ components/MobileMenu.jsx
@@ -1,25 +1,45 @@
-import Link from "next/link";
 import LinkButton from "./LinkButton";
+import { usePathname } from "next/navigation";
 
-function MobileMenu() {
+function MobileMenu({ isLoggedIn, setIsLoggedIn }) {
+  const pathName = usePathname();
+
   return (
     <div id="mobile-menu">
       <div className="space-y-1 px-2 pb-3 pt-2">
-        <LinkButton href="/" type="mobileMenu">
+        <LinkButton
+          href="/"
+          type="mobileMenu"
+          active={`${pathName === "/" ? "bg-black" : ""} `}
+        >
           Home
         </LinkButton>
-        <LinkButton href="/properties" type="mobileMenu">
+        <LinkButton
+          href="/properties"
+          type="mobileMenu"
+          active={`${pathName === "/properties" ? "bg-black" : ""} `}
+        >
           Properties
         </LinkButton>
-        <LinkButton href="/properties/add" type="mobileMenu">
-          Add Property
-        </LinkButton>
-        <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-          <span>Login or Register</span>
-        </button>
+        {isLoggedIn && (
+          <LinkButton
+            href="/properties/add"
+            type="mobileMenu"
+            active={`${pathName === "/properties/add" ? "bg-black" : ""} `}
+          >
+            Add Property
+          </LinkButton>
+        )}
+
+        {!isLoggedIn && (
+          <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
+            <span>Login or Register</span>
+          </button>
+        )}
       </div>
     </div>
   );
 }
 
 export default MobileMenu;
+// active={`${pathName === "/" ? "bg-black" : ""} `}
