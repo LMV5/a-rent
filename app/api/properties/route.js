67b7c85765f83d648c2db3c2c3de74c1d65d32c3
Marina <marina.lap08@gmail.com>
2024-04-16 13:01:00 +0200

    fetch data from the server

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,10 +1,15 @@
 import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+// GET /api/properties
 
 export const GET = async (request) => {
   try {
     await connectDB();
 
-    return new Response(JSON.stringify({ message: "Hello world" }), {
+    const properties = await Property.find({});
+
+    return new Response(JSON.stringify(properties), {
       status: 200,
     });
   } catch (error) {
@@ -13,3 +18,5 @@ export const GET = async (request) => {
     return new Response("Something Went Wrong", { status: 500 });
   }
 };
+
+// POST
