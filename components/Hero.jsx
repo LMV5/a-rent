03e414998c07a49b97f02a42c68436a33142ab37
Marina <marina.lap08@gmail.com>
2024-04-30 10:26:02 +0200

    create propertySearchForm, fetch search results from api

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,5 @@
+import PropertySearchForm from "./PropertySearchForm";
+
 function Hero() {
   return (
     <section className="bg-blue-300 py-20 mb-4">
@@ -10,39 +12,7 @@ function Hero() {
             Rent Smarter, Live Better: Renting Made Simple
           </p>
         </div>
-        <form className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center">
-          <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
-            <label htmlFor="location" className="sr-only">
-              Location
-            </label>
-            <input
-              type="text"
-              id="location"
-              placeholder="Enter Location (City, Country, Zip, etc)"
-              className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
-            />
-          </div>
-          <div className="w-full md:w-2/5 md:pl-2">
-            <label htmlFor="property-type" className="sr-only">
-              Property Type
-            </label>
-            <select
-              id="property-type"
-              className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
-            >
-              <option value="All">All</option>
-              <option value="Apartment">Apartment</option>
-              <option value="Hotel">Hotel</option>
-              <option value="Other">Other</option>
-            </select>
-          </div>
-          <button
-            type="submit"
-            className="md:ml-4 mt-4 md:mt-0 w-full md:w-auto px-6 py-3 rounded-lg bg-blue-700 text-white hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-500"
-          >
-            Search
-          </button>
-        </form>
+        <PropertySearchForm />
       </div>
     </section>
   );
--- components/LinkButton.jsx
+++ components/LinkButton.jsx
@@ -11,7 +11,7 @@ function LinkButton({ children, href, type, active, onClick, key }) {
     mobileMenu: "text-white block rounded-md px-3 py-2 text-base font-medium",
     btnForRenters: "text-white hover:bg-gray-700 rounded-md px-3 py-2",
     btnForOwners: "hover:bg-blue-300 rounded-md px-3 py-2",
-    btnBack: "text-blue-500 hover:text-blue-600 flex items-center",
+    btnBack: "text-blue-500 hover:text-blue-600 flex items-center mb-3",
     btnLogIn:
       "flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2",
   };
